e17adad226131e93b3abee57b9840890445c11d6
Zakariya7 <abdiladz1998@gmail.com>
2022-05-09 16:30:02 +0100

    Mini project commit so far

--- source/week2.py
+++ source/week2.py
@@ -1,71 +1,119 @@
-from cmath import log
 from itertools import count
-from usecase import logo_
 import os
+
+
 welcome_message = "Hello, welcome to Cafe London!"
-product_list = [" Americano Expresso", "Ginger chai Tea", "Regular Latte", "Caramel Latte", "Orange, papaya and manago smoothie", "Elder tree and apple juice"]
-main_menu_message='press [0] to exit app, press [1] for product menu '
-product_menu_message="[0] to return to main menu,\n[1] to show product menu,\n[2] to create a new product,\n[3] update existing product,\n[4] delete a product\n"
+main_menu_message='press [0] to exit app, press [1] for product menu, press [2] for courier menu '
+product_menu_message="[0] to return to main menu,\n[1] to show product list,\n[2] to create a new product,\n[3] to update existing product,\n[4] to delete a product\n"
+courier_menu_message='[0] to return to main menu,\n[1] to print courier list,\n[2] to create a new courier,\n[3] to update existing courier,\n[4] to delete a courier\n'
 
 
-print(logo_)
 print(welcome_message)
-print(main_menu_message)
 
+
+# read data (load the data from the text files to the lists)
 def load_data():
-    #read data from mini-project.txt
-    products_file=open('mini-project.txt')
+    #read data from Products.txt
+    products_file=open(r"C:\Users\User\Documents\mini.project\source\products.txt")
     products_list=products_file.readlines()
+    courier_file=open(r"C:\Users\User\Documents\mini.project\source\courierlist.txt")
+    courier_list=courier_file.readlines()
+    products_file.close()
+    courier_file.close()
+    return products_list,courier_list
 
-
+#write data (write the data into the respective file)
+def write_data(file_name,data):
+    f=open(file_name,'w')
+    f.writelines(data)
+    f.close()
 
 
 def main_menu():
-    user_input_main_menu = int(input())
-    if user_input_main_menu==0:
-        print("Exit the app \n")
-        with open('source/mini-project.txt', 'w') as file:
-            for product in product_list:
-                file.write(product)
-        # file.writelines(product_list)
-        exit()
-    else: 
-        product_menu()
+    while True:
+        global courier_list,product_list
+        user_input_main_menu = int(input(f'{main_menu_message} : '))
+        if user_input_main_menu==0:
+            print(product_list)
+            print(courier_list)
+            print("-=-------------------------------------------")
+            write_data(r"C:\Users\User\Documents\mini.project\source\products.txt",product_list)
+            write_data(r"C:\Users\User\Documents\mini.project\source\courierlist.txt",courier_list)    
+            print("Exit the app \n")
+            exit()
+        elif user_input_main_menu==1:
+            # print(product_menu_message)
+            product_menu()
+        elif user_input_main_menu==2:
+                couriers_menu()
+        
 
 
 def product_menu():
+    global product_list
     while True:
         user_options = int(input(product_menu_message))
         if user_options == 0:
             print("Return to main menu.\n")
-            print(main_menu_message)
-            main_menu()
+            return
             
         elif user_options == 1:
             os.system('cls')
-           
             print(product_list)
+           
         elif user_options==2:
             print('create new product\n')
-            1
-            product_list.append(input('enter your new product here\n'))
+            product_list.append(input('enter your new product here\n')+'\n')
             print(product_list)
-            os.system('cls')
         
         elif user_options==3:
             for count,value in enumerate(product_list):
                 print (count,value)
             choice1=int(input('what product do you want to change '))
             choice2=input('what do you want to add ')
-            product_list[choice1]=choice2
-            os.system('cls')
-        elif user_options ==4:
+            product_list[choice1]=choice2+'\n'
+
+        elif user_options==4:
             for count,value in enumerate(product_list):
-                print(count, value)
-                remove_item = int(input("what item number would you like to remove? "))
-                return product_list[remove_item]
-            os.system('cls')
+                print (count,value)
+            choice1=int(input('what product do you want delete '))
+            del product_list[choice1]    
+            
+
+def couriers_menu():
+    global courier_list
+    while True:
+        user_input_courier=int(input(courier_menu_message))
+        if user_input_courier==0:
+            print("Return to main menu.\n")
+            
+            return
+
+        elif user_input_courier== 1:
+                os.system('cls')
+                print(courier_list)  
+        elif user_input_courier==2:
+                print('create new courier\n')
+                courier_list.append(input('enter your new courier here\n')+'\n')
+                print(courier_list)
+        elif user_input_courier==3:
+                for count,value in enumerate(courier_list):
+                    print (count,value)
+                choice1=int(input('what courier number do you want to change '))
+                choice2=input('what courier name do you want to add ')
+                courier_list[choice1]=choice2+'\n'
+        elif user_input_courier==4:
+                for count,value in enumerate(courier_list):
+                    print (count,value)
+                choice1=int(input('what product do you want delete '))
+                del courier_list[choice1]   
+            
+           
+
+     
+
+
+product_list,courier_list=load_data()
 
 main_menu()
-
-
+import os
